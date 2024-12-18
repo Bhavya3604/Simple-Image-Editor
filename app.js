83498b8c42db64ae2350c3f2eddebd3b013eb279
Bhavya3604 <bhavyashah220@gmail.com>
2024-12-18 18:11:43 +0530

    JS done

--- app.js
+++ app.js
@@ -33,18 +33,99 @@ document.querySelector('.image-controls').style.display='block';
 document.querySelector('.preset-filters').style.display='block';
 }
 
-function applyFilter(){
-    let filterString=
-        "brightness("+brightnessSlider.value+"%)"+
-        " contrast("+contrastSlider.value+"%)"+
-        " grayscale("+grayscaleSlider.value+"%)"+
-        " sepia("+sepiaSlider.value+"%)"+
-        " saturate("+saturationSlider.value+"%)"+
-        " hue-rotate("+hueSlider.value+"%)";
+// This function is used to update the image
+// along with all the filter values
+function applyFilter() {
 
-    context.filter=filterString;
-    context.drawImage(image,0,0);
+    // Create a string that will contain all the filters
+    // to be used for the image
+    let filterString =
+        "brightness(" + brightnessSlider.value + "%" +
+        ") contrast(" + contrastSlider.value + "%" +
+        ") grayscale(" + grayscaleSlider.value + "%" +
+        ") saturate(" + saturationSlider.value + "%" +
+        ") sepia(" + sepiaSlider.value + "%" +
+        ") hue-rotate(" + hueSlider.value + "deg" + ")";
+
+    // Apply the filter to the image
+    context.filter = filterString;
+
+    // Draw the edited image to canvas
+    context.drawImage(image, 0, 0);
 }
+
+// A series of functions that handle the preset filters
+// Each of these will first reset the image
+// and then apply a certain parameter before
+// redrawing the image
+function brightenFilter() {
+    resetImage();
+    brightnessSlider.value = 130;
+    contrastSlider.value = 120;
+    saturationSlider.value = 120;
+    applyFilter();
+}
+
+function bwFilter() {
+    resetImage();
+    grayscaleSlider.value = 100;
+    brightnessSlider.value = 120;
+    contrastSlider.value = 120;
+    applyFilter();
+}
+
+function funkyFilter() {
+    resetImage();
+
+    // Set a random hue rotation everytime
+    hueRotateSlider.value =
+        Math.floor(Math.random() * 360) + 1;
+    contrastSlider.value = 120;
+    applyFilter();
+}
+
+function vintageFilter() {
+    resetImage();
+    brightnessSlider.value = 120;
+    saturationSlider.value = 120;
+    sepiaSlider.value = 150;
+    applyFilter();
+}
+
+function resetImage() {
+    brightnessSlider.value = 100;
+    contrastSlider.value = 100;
+    grayscaleSlider.value = 0;
+    hueRotateSlider.value = 0;
+    saturationSlider.value = 100;
+    sepiaSlider.value = 0;
+    applyFilter();
+}
+
+function saveImage() {
+    // Select the temporary element we have created for
+    // helping to save the image
+    let linkElement = document.getElementById('link');
+    linkElement.setAttribute(
+      'download', 'edited_image.png'
+    );
+
+    // Convert the canvas data to a image data URL
+    let canvasData = canvas.toDataURL("image/png")
+
+    // Replace it with a stream so that
+    // it starts downloading
+    canvasData.replace(
+      "image/png", "image/octet-stream"
+    )
+
+    // Set the location href to the canvas data
+    linkElement.setAttribute('href', canvasData);
+
+    // Click on the link to start the download 
+    linkElement.click();
+}
+
 
 
 
